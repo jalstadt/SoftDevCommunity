71f6826a407beb81c38c37b41944b01c531c4f7a
Jeff Alstadt <jalstadt@gmail.com>
2011-05-24 23:23:00 -0500

    init project

---
+++ FluentRibbonExample/FluentRibbonExample/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace FluentRibbonExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ FluentRibbonExample/FluentRibbonExample/Book.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentRibbonExample
+{
+    public class Book
+    {
+        public Book(string name, string isbn, string price)
+        {
+            Name = name;
+            ISBN = isbn;
+            Price = price;
+
+        }
+
+        private string _name;
+        public string Name
+        {
+            get 
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                }
+            }
+        }
+        private string _isbn;
+        public string ISBN
+        {
+            get
+            {
+                return _isbn;
+            }
+            set
+            {
+                if (_isbn != value)
+                {
+                    _isbn = value;
+                }
+            }
+        }
+        private string _price;
+        public string Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                }
+            }
+        }
+    }
+}
